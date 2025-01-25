23256391d6ef3cee3e76b5722bfbeb3c5fca5ab6
Hauharu <trunghauu71@gmail.com>
2025-01-25 14:34:18 +0700

    update

--- bookingfood_mobileapp/Components/Home/Styles.js
+++ bookingfood_mobileapp/Components/Home/Styles.js
@@ -1,5 +1,35 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+    container:{
+        flex:1,
+        padding:15,
+    },
+    container_acc:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:16,
+        backgroundColor:''
+    },
+    coverAvatar: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
+        marginBottom: 16,
+    },
+    avatar: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        borderWidth: 2,
+        borderColor: '#ffffff',
+        marginBottom: 16,
+    },
+    text: {
+        fontSize: 16,
+        color: '#333333',
+        marginBottom: 8,
+    },
+
 })
