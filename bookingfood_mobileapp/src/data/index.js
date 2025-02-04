a6fe0ad3b46fc61bbd3b74feaefdc957998899ae
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2025-02-04 12:03:23 +0700

    Delete bookingfood_mobileapp directory

--- bookingfood_mobileapp/src/data/index.js
+++
@@ -1,5 +0,0 @@
-import RESTAURANTS from "./restaurants";
-import CATEGORIES from "./categories";
-import FOODS from "./foods";
-import VOUCHERS from "./vouchers";
-export { RESTAURANTS, CATEGORIES, FOODS, VOUCHERS }
